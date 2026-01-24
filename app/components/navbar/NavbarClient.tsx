b32f51db00a1816315bf31aee135d54033ea3a9a
Damian Jankov <jankovdamian@gmail.com>
2026-01-24 10:56:59 +0100

    Add links to each site

--- app/components/navbar/NavbarClient.tsx
+++ app/components/navbar/NavbarClient.tsx
@@ -1,16 +1,16 @@
 "use client";
 
 import { Navbar, Container, Nav } from "react-bootstrap";
-
+import Link from "next/link";
 
 export default function NavbarClient() {
     return (
         <Navbar bg="dark" data-bs-theme="dark">
             <Container>
                 <Nav className="mx-auto">
-                    <Nav.Link>Tasks</Nav.Link>
-                    <Nav.Link>Work reports</Nav.Link>
-                    <Nav.Link>Workouts</Nav.Link>
+                    <Nav.Link as={Link} href="/tasks">Tasks</Nav.Link>
+                    <Nav.Link as={Link} href="/work-reports">Work reports</Nav.Link>
+                    <Nav.Link as={Link} href="/workouts">Workouts</Nav.Link>
                 </Nav>
             </Container>
         </Navbar>
