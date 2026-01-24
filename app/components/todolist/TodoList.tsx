e47e7ec117b04b0a18e506abf67ba313eaf4c4a6
Damian Jankov <jankovdamian@gmail.com>
2026-01-24 12:00:39 +0100

    Todolist has a bootstrap styling now

--- app/components/todolist/TodoList.tsx
+++ app/components/todolist/TodoList.tsx
@@ -2,6 +2,10 @@
 
 import { useState, useEffect } from "react";
 import { Todo } from "../../interfaces/Todo"
+import InputGroup from "react-bootstrap/esm/InputGroup";
+import Button from "react-bootstrap/esm/Button";
+import Form from "react-bootstrap/esm/Form";
+import { Col, Container, Row } from "react-bootstrap";
 
 export default function TodoList({ injectedTodos }: { injectedTodos: Todo[] }) {
 
@@ -25,24 +29,55 @@ export default function TodoList({ injectedTodos }: { injectedTodos: Todo[] }) {
         setTodos([...todos, newTodo]);
     }
 
+    function markTodo(id: number) {
+        setTodos(todos.map(todo => {
+            if (todo.id === id) {
+
+                if (todo.done) {
+                    return { ...todo, done: false };
+                } else {
+                    return { ...todo, done: true };
+                }
+
+            }
+            return todo;
+        }));
+    }
+
     return (
-        <div>
-            <h2>Todo List</h2>
-            <input
-                type="text"
-                placeholder="New todo"
-                value={inputTextValue}
-                onChange={(e) => setInputTextValue(e.target.value)}
-            />
-            <button onClick={() => addTodo(inputTextValue)}>Add Todo</button>
-            <ul>
-                {todos.map((todo) => (
-                    <li key={todo.id}>
-                        {todo.content} - Status: {todo.done ? "Done" : "Not Done"}
-                        <button onClick={() => deleteTodo(todo.id)}>Delete</button>
-                    </li>
-                ))}
-            </ul>
-        </div>
+        <Container>
+            <Row className="mt-3">
+                <Col>
+                    <InputGroup className="mb-3">
+                        <Form.Control
+                            placeholder="Recipient's username"
+                            aria-label="Recipient's username"
+                            aria-describedby="basic-addon2"
+                            value={inputTextValue}
+                            onChange={(e) => setInputTextValue(e.target.value)}
+                        />
+                        <Button variant="outline-secondary" id="button-addon2" onClick={() => addTodo(inputTextValue)}>
+                            Add Todo
+                        </Button>
+                    </InputGroup>
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    <ul>
+                        {todos.map((todo) => (
+                            <li key={todo.id}>
+                                <InputGroup className="mb-3">
+                                    <InputGroup.Checkbox aria-label="Checkbox for following text input" checked={todo.done} onChange={(e) => markTodo(todo.id)} />
+
+                                    <Form.Control aria-label="Text input with checkbox" readOnly={true} value={todo.content} onChange={(e) => { }} />
+                                    <Button variant="outline-primary" onClick={() => deleteTodo(todo.id)}>Delete</Button>
+                                </InputGroup>
+                            </li>
+                        ))}
+                    </ul>
+                </Col>
+            </Row>
+        </Container>
     );
 }
