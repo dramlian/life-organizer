0fd4742ddbfcc615b64dc01f4dff0c5443c047f8
Damian Jankov <jankovdamian@gmail.com>
2026-01-25 18:08:37 +0100

    Updated the daily constants

--- app/lib/constants.tsx
+++ app/lib/constants.tsx
@@ -1,8 +1,11 @@
 import { Todo } from "../interfaces/Todo";
 
 const defaultTasks: Todo[] = [
-    { id: 1, content: "Sample Task 1", done: false },
-    { id: 2, content: "Sample Task 2", done: false },
+    { id: 1, content: "Tvarohy", done: false },
+    { id: 2, content: "Kreatin", done: false },
+    { id: 3, content: "Duolingo", done: false },
+    { id: 4, content: "Citat", done: false },
+    { id: 5, content: "Cvicit/Pohyb", done: false },
 ];
 
 export default defaultTasks;
