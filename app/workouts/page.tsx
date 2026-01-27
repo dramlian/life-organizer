04c23e5de9347636ab99279b00f4caace104c5e2
Damian Jankov <jankovdamian@gmail.com>
2026-01-27 14:31:47 +0100

    UI of the workout component done

--- app/workouts/page.tsx
+++ app/workouts/page.tsx
@@ -1,23 +1,61 @@
-import { Col, Row, Container } from "react-bootstrap";
+'use client';
+
+import { useState } from "react";
+import { Col, Row, Container, ListGroup, InputGroup, Button, Form } from "react-bootstrap";
+import RichTextEditor from "../components/richtexteditor/RichTextEditor";
+import Stopwatch from "../components/stopwatch/Stopwatch";
 
 export default function Workouts() {
+
+    const [inputTextValue, setInputTextValue] = useState<string>("");
+    const [html, setHtml] = useState<string>("");
+
+    function addWorkout(content: string) {
+        // Placeholder function for adding a workout
+        console.log("Adding workout:", content);
+        setInputTextValue(content);
+    }
+
     return (
         <Container>
-            <Row>
-                <Col md={4} style={{ height: '100vh', backgroundColor: '#fc0000' }}>
-
+            <Row className="mt-3 gap-3">
+                <Col md={3} className="pt-3 border rounded " style={{ height: '80vh' }} >
+                    <InputGroup className="mb-3">
+                        <InputGroup.Checkbox aria-label="Placeholder checkbox" style={{ visibility: 'hidden' }} disabled />
+                        <Form.Control
+                            placeholder="Workout name"
+                            aria-label="Workout name"
+                            aria-describedby="basic-addon2"
+                            value={inputTextValue}
+                            onChange={(e) => setInputTextValue(e.target.value)}
+                        />
+                        <Button variant="outline-secondary" id="button-addon2" onClick={() => addWorkout(inputTextValue)}>
+                            Add Workout
+                        </Button>
+                    </InputGroup>
+                    <ListGroup>
+                        <ListGroup.Item  >
+                            Link 1
+                        </ListGroup.Item>
+                        <ListGroup.Item>
+                            Link 2
+                        </ListGroup.Item>
+                        <ListGroup.Item>
+                            This one is a button
+                        </ListGroup.Item>
+                    </ListGroup>
                 </Col>
-                <Col md={8} style={{ height: '100vh', backgroundColor: '#7700ff' }}>
+                <Col className="pt-3 border rounded " style={{ height: '80vh', overflowY: 'scroll' }} >
                     <Row>
-                        <p>RTE</p>
+                        <Stopwatch />
                     </Row>
                     <Row>
-                        <p>RTE</p>
+                        <RichTextEditor initialHtml={html} onChange={setHtml} />
                     </Row>
                 </Col>
             </Row>
 
 
-        </Container>
+        </Container >
     );
 }
