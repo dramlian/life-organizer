b491f4cf9f70fb70ca3d4458f3b23452b9d72379
Damian Jankov <jankovdamian@gmail.com>
2026-02-22 17:21:06 +0100

    Added middleware route protection

--- middleware.ts
+++ middleware.ts
@@ -3,5 +3,5 @@ import { withAuth } from "next-auth/middleware";
 export default withAuth;
 
 export const config = {
-  matcher: ["/tasks/:path*", "/workreports/:path*", "/workouts/:path*"],
+  matcher: ["/tasks/:path*", "/workreports/:path*", "/workouts/:path*", "/notes/:path*"],
 };
