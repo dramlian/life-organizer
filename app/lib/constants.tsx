978337d974f6704631b1ac06664a424f7a9c6595
Damian Jankov <jankovdamian@gmail.com>
2026-03-16 12:42:55 +0100

    Updated constants

--- app/lib/constants.tsx
+++ app/lib/constants.tsx
@@ -6,7 +6,7 @@ const defaultTasks: Todo[] = [
     { id: 3, content: "Duolingo", done: false },
     { id: 4, content: "Citat", done: false },
     { id: 5, content: "Cvicit/Pohyb", done: false },
-    { id: 6, content: "Sivy respirator", done: false },
+    { id: 6, content: "Dotnet", done: false },
 ];
 
 const sampleReport: string = `<b>Yesterday's Catch Up</b><p>- </p><b>Todo for Today</b><p>- </p>`;
